fe795b88847f1ef55ccab1710003201879e36bae
alisue <alisue@hashnote.net>
2011-03-19 13:38:47 +0900

    Modify package installer

--- utils/package.sh
+++ utils/package.sh
@@ -4,4 +4,8 @@
 #
 yes | pip install markdown whoosh Pygments PyYAML Akismet
 yes | pip install django django-compress django-reversetag django-pagination
-yes | pip install django-modify-history django-markitup-widget django-codemirror-widget
+# Install from github
+yes | pip install -e git://github.com/alisue/django-modify-history
+yes | pip install -e git://github.com/alisue/django-markitup-widget
+yes | pip install -e git://github.com/alisue/django-codemirror-widget
+yes | pip install -e git://github.com/alisue/django-qwert
