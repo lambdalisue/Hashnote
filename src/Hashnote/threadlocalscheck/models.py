250a14b29d8ef560cbb551c29dd3d21a5b66861e
alisue <alisue@hashnote.net>
2011-03-21 15:39:54 +0900

    Admin

--- src/Hashnote/threadlocalscheck/models.py
+++ src/Hashnote/threadlocalscheck/models.py
@@ -5,8 +5,11 @@
 # Date:        2011/03/21
 #
 from django.db import models
+from django.contrib.admin import site
 
 class Threadlocals(models.Model):
     threadlocals_id = models.CharField('threadlocals id', max_length=255)
     ip_address      = models.IPAddressField('ip address')
     created_at      = models.DateTimeField('date created', auto_now_add=True)
+    
+site.register(Threadlocals)
